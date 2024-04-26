7a5a95cf955f22a44ee025a01ab693b6566a22be
Almansur342 <abdullahalmansur2002@gmail.com>
2024-04-26 20:09:10 +0600

    navbar and footer added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import { Link, NavLink, } from "react-router-dom";
+import logo from '../../../assets/logo.png'
+
+
 
 const Navbar = () => {
+  // const {user,logOut} = useContext(AuthContext);
+  // const navigate = useNavigate();
+  // const handleLogOut = () =>{
+  //   logOut()
+  //   .then(result => {
+  //     console.log(result.user);
+  //     navigate('/');
+  //   })
+  //   .catch(error =>{
+  //     console.error(error);
+  //   })
+  // }
+
+  const navLink = <div className="flex items-center">
+    <li className="font-bold text-base hover:text-[#b18b5e] hover:border-b border-b-[#b18b5e]"><NavLink to='/' className= {({ isActive }) => isActive ? '  text-[#b18b5e]' : 'text-[#131313CC] hover:text-[#b18b5e]'}>Home</NavLink></li>
+
+    <li className="font-bold text-base hover:text-[#b18b5e] shadow-none hover:border-b border-b-[#b18b5e] bg-white"><NavLink to='/updateProfile' className={({ isActive }) => isActive ? 'text-[#b18b5e] ' :'hover:text-[#b18b5e] text-[#131313CC]'}>All Art & craft Items</NavLink></li>
+
+    <li className="font-bold text-base hover:text-[#b18b5e] shadow-none hover:border-b border-b-[#b18b5e] bg-white"><NavLink to='/updateProfile' className={({ isActive }) => isActive ? 'text-[#b18b5e] ' :'hover:text-[#b18b5e] text-[#131313CC]'}>All Art & craft Items</NavLink></li>
+
+    <li className="font-bold text-base hover:text-[#b18b5e] shadow-none hover:border-b border-b-[#b18b5e] bg-white"><NavLink to='/updateProfile' className={({ isActive }) => isActive ? 'text-[#b18b5e] ' :'hover:text-[#b18b5e] text-[#131313CC]'}>All Art & craft Items</NavLink></li>
+
+    
+
+    {/* { user && <>
+      <li><NavLink to='/contactUs' className={({ isActive }) => isActive ? 'border border-green-600 text-[#23BE0A]' : 'text-[#131313CC]'}>Contact Us</NavLink></li> */}
+ 
+  </div>
   return (
-    <div>
-      
+    <div className="navbar max-w-6xl mx-auto mt-5">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </div>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2  w-52">
+            {navLink}
+          </ul>
+        </div>
+        <div>
+          <img className="w-44" src={logo} alt="" />
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {navLink}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        
+        <div>
+          <Link to='/login'>
+            <button className="px-3 font-semibold text-base text-white bg-[#b18b5e] rounded py-2">Login</button>
+          </Link>
+          <Link to='/login'>
+            <button className="px-3 ml-2 font-semibold text-base text-white bg-[#b18b5e] rounded py-2">Register</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
